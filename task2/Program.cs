f89bd4406b8fd2a8d4a2f08b2966e2f654989a16
Oleg Suroegin <suroegin.oleg.07122004@gmail.com>
2025-03-12 13:01:57 +0300

    add task2

---
+++ task2/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] arr = new int [10];
+Random rand = new Random();
+for (int i = 0; i < 10; i++)
+    arr[i] = rand.Next(-10, 11);
+foreach (int i in arr)
+    Console.Write($"{i}\t");
+Console.WriteLine();
+for (int i = 0; i < 10; i++)
+{
+    arr[i] *= -1;
+    Console.Write($"{arr[i]}\t");
+}
