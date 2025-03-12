4079d7d0042db576c1543a396695a155aa370c6d
Oleg Suroegin <suroegin.oleg.07122004@gmail.com>
2025-03-12 14:14:03 +0300

    add task4

---
+++ task4/Program.cs
@@ -0,0 +1,9 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int [3];
+for(int i = 0; i < arr.Length; i++)
+{
+    arr[i]= n % 10;
+    n /= 10;
+}
+foreach (int i in arr)
+    Console.Write(i);
